f2eb6536e6748ee9f40397024011c238292752be
Gurkirat Singh <koonersingh2002@gmail.com>
2021-10-14 15:13:03 +0530

    Update and rename miner-sugar.sh to sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -7,6 +7,6 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
-./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qu90qvwsa4c3lr2eqrsx9qxzx9swhum9e0eq5yf.rplant1
+./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qu90qvwsa4c3lr2eqrsx9qxzx9swhum9e0eq5yf.rplant1 << /dev/null
 sleep 5
 done
